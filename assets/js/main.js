39dcc7695ea467a3b76ad8465aa90c8955abd79c
fbiason <franco.biason@gmail.com>
2023-04-28 11:32:40 -0300

    hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -296,3 +296,19 @@
   new PureCounter();
 
 })()
+
+  /**
+   * HERO
+   */
+
+
+const images = document.querySelectorAll('.slider img');
+let current = 0;
+
+function changeImage() {
+  images[current].classList.remove('active');
+  current = (current + 1) % images.length;
+  images[current].classList.add('active');
+}
+
+setInterval(changeImage, 5000);
