9dc13e710768136a2d3f4964a9cd3088e3352361
biason franco <franco.biason@gmail.com>
2023-06-13 19:47:59 -0300

    13.6.23

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,18 +3,18 @@ const btn = document.getElementById("button");
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
-  btn.value = "Sending...";
+  btn.value = "Enviando...";
 
   const serviceID = "default_service";
   const templateID = "template_d44kb4v";
 
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
-      btn.value = "Send Email";
-      alert("Sent!");
+      btn.value = "Enviando Email";
+      alert("¡Mensaje Enviado!");
     },
     (err) => {
-      btn.value = "Send Email";
+      btn.value = "Enviando Email";
       alert(JSON.stringify(err));
     }
   );
