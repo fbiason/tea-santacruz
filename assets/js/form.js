4486e23ee283c62e4a92cd4a2f109cd85a2f004e
José Fernando Gomez <101230011+Gomez-Dev@users.noreply.github.com>
2023-06-14 18:59:22 -0300

    14-06/19

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,7 +3,7 @@ const btn = document.getElementById("button");
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
-  btn.value = "Enviando...";
+  btn.value = "Sending...";
 
   const serviceID = "default_service";
   const templateID = "template_d44kb4v";
@@ -11,10 +11,10 @@ document.getElementById("form").addEventListener("submit", function (event) {
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
       btn.value = "Send Email";
-      // alert("Sent!");
+      alert("Sent!");
     },
     (err) => {
-      btn.value = "Enviando Email";
+      btn.value = "Send Email";
       alert(JSON.stringify(err));
     }
   );
